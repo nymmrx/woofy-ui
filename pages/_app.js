f4953ea56686fe794a6534d1a3b475a518554798
nymmrx <nymmrx@protonmail.com>
2021-05-11 20:08:10 +0200

    chore: fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Web3ContextProvider } from "../helpers/web3";
 
-import "../styles/globals.css";
-
-import "@fontsource/inter/200.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/800.css";
+import "../styles/globals.scss";
 
 const theme = extendTheme({
   fonts: {
